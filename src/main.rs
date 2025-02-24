0c8148f7c046a707335d9bcc4f6fe5a1a41a6675
ThaRealSteveJobs <mason+jobs@merit.systems>
2025-02-24 16:50:51 +0000

    feat: allow user to specify repo path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use merit_cli_demo::run;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    
+    let repo_path = env::args().nth(1);
+    run(repo_path).await
 }
